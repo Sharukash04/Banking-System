00df9d724a017dbbd50531c5ce8d81fc457e1e07
SHARUKASH <sharukash01@gmail.com>
2026-07-31 22:18:34 +0530

    Adding the bank.py and storage

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-print("="*15 + " Quickbasket " + "="*15)
+print("="*15 + " TN BANK " + "="*15)
 print()
 print()
-print("\tWELCOME TO QUICKBASKET")
-print("Fresh essentials delivered in just 30 minutes")
-
+print(" "*10 +"WELCOME TO TN BANK")
+print(" "*5+"Your trusted financial partner")
 content='''
-1. Login
-2. Register
-3. Browse Categories
-4. Search Product
-5. View Cart
-6. Checkout
-7. My Orders
-8. My Profile
-9. Logout
-10. Exit'''
-print(content)
+1. Create Account
+2. Deposit
+3. Withdraw
+4. Balance Check
+5. Display Accounts
+6. Change PIN
+7. Close Account
+8. Exit'''
+print()
 while True:
+    print(content)
     choice=int(input("Enter the number regarding to your choice:"))
 
     if choice == 1:
-        from login import login
-        login()
-
-    elif choice == 2:
         from Register import register
         register()
+       
+
+    elif choice == 2:
+        from login import login
+        login()
