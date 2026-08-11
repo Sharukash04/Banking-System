ca9ddf3ba964413b40e044825846253cfe5643f8
SHARUKASH <sharukash01@gmail.com>
2026-08-11 21:02:18 +0530

    account_dao.py

--- Banking System/account_dao.py
+++ Banking System/account_dao.py
@@ -1,8 +1,26 @@
-# storage.py
+from storage import accounts
 
-accounts = {}
 
-add_account(account)
-find_account(account_id)
-get_all_accounts()
-delete_account(account_id)
+def add_account(account):
+    accounts[account.account_id] = account
+    return True
+
+
+def find_account(account_id):
+    return accounts.get(account_id)
+
+
+def account_exists(account_id):
+    return account_id in accounts
+
+
+def get_all_accounts():
+    return accounts.values()
+
+
+def delete_account(account_id):
+    if account_id in accounts:
+        del accounts[account_id]
+        return True
+
+    return False
